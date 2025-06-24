94255bc515b987b33792cc730c1145a1477aacc4
Htoo <197538480+TeAnKe@users.noreply.github.com>
2025-06-24 01:50:22 -0700

    added a reset button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var counter = 0;
 
-function changeText() {
+function increment() {
   counter = counter + 1;
   document.getElementById("count").innerText = counter;
+}
+
+function reset() {
+    counter = 0;
+    document.getElementById("count").innerText = counter;
 }
