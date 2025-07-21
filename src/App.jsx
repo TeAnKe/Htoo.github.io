5d84499bc2fd03f5c08bfe365884f39ba8dce66e
Htoo <197538480+TeAnKe@users.noreply.github.com>
2025-07-20 19:18:18 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState } from 'react';
- 
+import { Button, Typography, Container, Box } from '@mui/material';
  
 function App() {
   const [count, setCount] = useState(0);
@@ -11,8 +11,46 @@ function App() {
  
   return (
     <>
-      <h1>🎉React🎉</h1>
-      <button onClick={handleClick}>Count is {count}</button>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Container >
+          <Typography
+            variant="h3"
+            gutterBottom
+            sx={{
+              textAlign: 'center',
+              color: '#2c3e50',
+              // fontSize: '3rem',
+              marginBottom: '20px',
+              animation: `pop-in 0.6s ease-out`,
+              '@keyframes pop-in': {
+                '0%': { transform: 'scale(0.5)', opacity: 0 },
+                '100%': { transform: 'scale(1)', opacity: 1 },
+              },
+            }}
+          >
+            🎉React + Material UI🎉
+          </Typography >
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleClick}
+            sx={{
+              mt: 3,
+              '&:hover': {
+                backgroundColor: `#1a252f`
+              }
+            }}
+          >
+            Count is {count}
+          </Button >
+        </Container>
+      </Box >
     </>
   )
 }
